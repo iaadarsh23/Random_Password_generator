198490b0ba886f210aa92d2991b9eec9be6acf7a
Adarsh <adarshtrip2306@gmail.com>
2024-11-14 21:59:42 +0530

    right side container styling done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,27 @@
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+
   content: [
     "./public/**/*.{html,js}",
     "./src/**/*.{html,js}",
     "./*.{html,js}"
   ],
-  
+
+
+
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        rotate: 'rotate 6s linear infinite',
+      },
+      keyframes: {
+        rotate: {
+          '0%': { transform: 'rotate(0deg) scale(20)' },
+          '100%': { transform: 'rotate(-360deg) scale(20)' },
+        },
+      },
+    },
   },
   plugins: [],
 }
